23c0208dc7896581ecd6f0fc1cfc4ae794856ab9
Darko Kukovec <darko@infinum.hr>
2020-12-01 21:05:21 +0100

    Add prettier

--- test/01.test.js
+++ test/01.test.js
@@ -4,23 +4,23 @@ const { A01, B01 } = require('../lib/01');
 const input = fs.readFileSync('./data/01.txt', 'utf-8');
 
 describe('01', () => {
-  describe('A', () => {
-    it('Example 1', () => {
-      expect(A01('1721\n979\n366\n299\n675\n1456')).toBe(514579);
-    });
+	describe('A', () => {
+		it('Example 1', () => {
+			expect(A01('1721\n979\n366\n299\n675\n1456')).toBe(514579);
+		});
 
-    it('Solution', () => {
-      expect(A01(input)).toBe(567171);
-    });
-  });
+		it('Solution', () => {
+			expect(A01(input)).toBe(567171);
+		});
+	});
 
-  describe('B', () => {
-    it('Example 1', () => {
-      expect(B01('1721\n979\n366\n299\n675\n1456')).toBe(241861950);
-    });
+	describe('B', () => {
+		it('Example 1', () => {
+			expect(B01('1721\n979\n366\n299\n675\n1456')).toBe(241861950);
+		});
 
-    it('Solution', () => {
-      expect(B01(input)).toBe(212428694);
-    });
-  });
+		it('Solution', () => {
+			expect(B01(input)).toBe(212428694);
+		});
+	});
 });
